59041d9ef9eb9ed1f3347cd602fb1ef46d8c9604
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-03-13 08:53:23 -0400

    setting migrations and ensuring we have a db always

---
+++ backend/knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+const fs = require("fs");
+
+const readFileSync = (filename) => fs.readFileSync(filename).toString("utf8");
+
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DATABASE_HOST || "localhost",
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_DB,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+        ? readFileSync(process.env.DATABASE_PASSWORD)
+        : null,
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,31 @@
-const knex = require('knex');
-const { database } = require('./config');
+// const knex = require('knex');
+// const { database } = require('./config');
 
-module.exports = knex({
-  client: 'mysql2',
-  connection: database,
-});
+// module.exports = knex({
+//   client: 'mysql2',
+//   connection: database,
+// });
+
+
+//test connections 
+
+const knex = require('knex');
+
+const conf = require('../knexfile')
+
+const connections = knex(conf.development)
+
+module.exports = connections;
+
+
+
+// knex.raw('SELECT 1')
+//   .then(() => {
+//     console.log('Database connection successful');
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to database:', err);
+//   })
+//   .finally(() => {
+//     knex.destroy();
+//   });
