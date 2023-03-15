24a455446032b1feeebcb5e2a4824f9864c61d2d
Jarvis <jarvis@MacBook-Air-de-teuddy.local>
2023-03-15 13:13:35 -0400

    backen now works with .env instead compose env

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,12 +1,64 @@
 require('dotenv').config()
+const fs = require("fs");
+const readFileSync = (filename) => fs.readFileSync(filename).toString("utf8");
+
 
 // Constants
 module.exports = {
 
-
   port: process.env.PORT || 8080,
   environmnet: process.env.NODE_ENV || 'development',
-  // if you're not using docker compose for local development, this will default to 8080
-  // to prevent non-root permission problems with 80. Dockerfile is set to make this 80
-  // because containers don't have that issue :)
+  databasesConfigurations:{
+          development: {
+            client: 'mysql',
+            connection: {
+              host: process.env.DATABASE_HOST || "localhost",
+              port: process.env.DATABASE_PORT,
+              database: process.env.DATABASE_DB,
+              user: process.env.DATABASE_USER,
+              password: process.env.DATABASE_PASSWORD
+                ? readFileSync(process.env.DATABASE_PASSWORD)
+                : null,
+            },
+            migrations:{
+              directory:__dirname + '/../migrations'
+            },
+            seeds:{
+              directory:__dirname + '/../seeds'
+            }
+            
+          },
+
+          staging: {
+            client: 'postgresql',
+            connection: {
+              database: 'my_db',
+              user:     'username',
+              password: 'password'
+            },
+            pool: {
+              min: 2,
+              max: 10
+            },
+            migrations: {
+              tableName: 'knex_migrations'
+            }
+          },
+
+          production: {
+            client: 'postgresql',
+            connection: {
+              database: 'my_db',
+              user:     'username',
+              password: 'password'
+            },
+            pool: {
+              min: 2,
+              max: 10
+            },
+            migrations: {
+              tableName: 'knex_migrations'
+            }
+          }
+      }
 };
